9786fda34d185b8e08e0a0f3dfe53a204fe90079
aryapackiyanath <142537816+aryapackiyanath@users.noreply.github.com>
2023-08-17 13:22:01 -0700

    Delete bank_system.py
    duplicate file

--- bank_system.py
+++
@@ -1,6 +0,0 @@
-class bank_account:
-    def __init__(self, fname, lname, savings_amount, checking_amount,):
-        self.fname = fname
-        self.lname = lname
-        self.savings_amount = savings_amount
-        self.checkingamount = checking_amount
